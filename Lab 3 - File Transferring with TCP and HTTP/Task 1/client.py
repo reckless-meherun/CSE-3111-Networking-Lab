8d51d76dc8a5a5dd7efc2bf59592281df7954f19
reckless-meherun <meherun2001@gmail.com>
2024-02-08 21:33:51 +0600

    Lab 3 uploaded

---
+++ Lab 3 - File Transferring with TCP and HTTP/Task 1/client.py
@@ -0,0 +1,99 @@
+import socket
+import pickle
+import os
+import tqdm
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 9090
+sock: socket.socket
+
+def list(sock):
+    print('\nFiles:')
+    response = sock.recv(1024).decode()
+    print(f'{response}\n')
+
+def upload(sock):
+    response = sock.recv(1024).decode()
+    print(response)
+    filename = input('Enter filename: ')
+    filepath = os.path.join('./client_files',filename)
+
+    try:
+        with open(filepath,'rb') as f:
+            content = f.read()
+        response = {
+            'status': 'OK',
+            'filename': filename,
+            'content': content
+        }
+        success = True
+
+    except FileNotFoundError:
+        print('File does not exist')
+        response = {
+            'status': 'ERROR',
+        }
+        success = False
+
+    response = pickle.dumps(response)
+    sock.send(f'{len(response)}'.encode())
+    sock.recv(1024).decode()
+    sock.send(response)
+
+    if success: print(f'Uploaded {filepath}')
+    else: print('Try again.')
+    
+def download(sock):
+    response = sock.recv(1024).decode()
+    print(response)
+    filename = input('Enter filename: ')
+    sock.send(filename.encode())
+
+    length = sock.recv(1024).decode()
+    sock.send('OK'.encode())
+
+    response = b''
+    got_len = 0
+    with tqdm.tqdm(total=int(length), unit='B', unit_scale=True, unit_divisor=1024) as progress_bar:
+        while got_len < int(length):
+            data = sock.recv(1024)
+            response += data
+            got_len += len(data)
+            progress_bar.update(len(data))
+
+    response = pickle.loads(response)
+    sock.send('RECV'.encode())
+
+    if response['status'] == 'ERROR':
+        print('File does not exist')
+        return
+
+    filepath = os.path.join('./client_files', filename)
+
+    with open(filepath, 'wb') as f:
+        f.write(response['content'])
+
+    print(f'Downloaded {filepath}')
+
+def client_init():
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect((HOST,PORT))
+
+    while True:
+        options = sock.recv(1024).decode()
+        print(options)
+
+        selected = input('-> ')
+        sock.send(selected.encode())
+
+        match selected:
+            case '1':
+                list(sock)
+            case '2':
+                upload(sock)
+            case '3':
+                download(sock)
+
+    
+
+client_init()
---
+++ Lab 3 - File Transferring with TCP and HTTP/Task 2/client.py
@@ -0,0 +1,43 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+server_url = 'http://172.29.96.128:12349'
+
+while True:
+    options = '''Select one :
+    1. list
+    2. upload
+    3. download
+    '''
+        
+    print(options)
+
+    selected_operation = input()   
+    
+    match selected_operation:
+        case '1': #list
+            response = requests.get(f'{server_url}/list')
+            files = response.json()
+            print("Available Files : ")            
+            for file in files:
+                print(file)
+                
+        case '2': #upload
+            filename = input('Enter the filename: ')        
+            with open(filename, 'rb') as file:
+                response = requests.post(f'{server_url}/upload/{filename}', data=file)
+            print(response.text)
+
+        case '3': # download
+            filename = input('Enter the filename: ')
+            response = requests.get(f'{server_url}/download/{filename}')
+            if response.status_code == 200:
+                with open(filename, 'wb') as file:
+                    file.write(response.content)
+                print('Successfully downloaded the file!')
+            else:
+                print('Sorry! Failed to download file!')
+ 
+            
+
